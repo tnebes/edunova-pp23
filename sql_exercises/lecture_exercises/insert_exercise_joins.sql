39bcc37f450a9719b6022f34e7d66dc2dbe56b4f
tnebes <tomislav.nebes@gmail.com>
2021-05-19 19:50:48 +0200

    Inserting and updating using subqueries

--- sql_exercises/lecture_exercises/insert_exercise_joins.sql
+++ sql_exercises/lecture_exercises/insert_exercise_joins.sql
@@ -20,3 +20,23 @@ insert into grupa(naziv, smjer) values
 -- using subqueries
 insert into grupa(naziv, smjer) values
 ("test item", (select sifra from smjer where lower(naziv) like "%php%"));
+
+select a.naziv as grupa, b.naziv as smjer,
+a.predavac
+from grupa a inner join smjer b
+on a.smjer = b.sifra;
+
+insert into osoba(ime, prezime)
+values ("Marija", "Zimska");
+
+insert into predavac(osoba)
+values ((select sifra from osoba where lower(ime) like "%marija%" and lower(prezime) like "%zimska%"));
+
+update grupa set predavac = (
+select a.sifra 
+from predavac a inner join osoba b 
+on a.osoba = b.sifra
+where b.ime like "%marija%" and b.prezime like "zimska"
+) where lower(grupa.naziv) like "%pp23%";
+
+
