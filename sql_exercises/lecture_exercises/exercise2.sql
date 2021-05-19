7d036ca81f2af0798ea3871b52dfba4e7bd4cf30
tnebes <tomislav.nebes@gmail.com>
2021-05-19 20:56:21 +0200

    Searching the classic models

--- sql_exercises/lecture_exercises/exercise2.sql
+++ sql_exercises/lecture_exercises/exercise2.sql
@@ -1,1 +1,5 @@
-use classimodels;
+use classicmodels;
+
+select a.productName as 'Product name', b.productLine as 'Product line'
+from products a inner join productlines b
+on a.productLine = b.productLine;
