c897951fa8586e11ffa64891719d85b013593be0
tnebes <tnebes@github.com>
2021-05-19 19:24:37 +0200

    Added lecture exercises

---
+++ sql_exercises/lecture_exercises/insert_exercise.sql
@@ -0,0 +1,9 @@
+use edunovapp23;
+
+select * from smjer;
+
+-- unesite novi smjer serviser
+-- s minimalnim skupom atributa
+
+insert into smjer(naziv, trajanje, certifikat, cijena)
+	values ("Serviser", 50, 1, 1999.99);
