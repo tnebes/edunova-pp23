1236d251767c65274b6d2ffb5b07376312f0911e
tnebes <tomislav.nebes@gmail.com>
2021-05-19 19:42:12 +0200

    subqueries

--- sql_exercises/lecture_exercises/insert_exercise_joins.sql
+++ sql_exercises/lecture_exercises/insert_exercise_joins.sql
@@ -13,3 +13,10 @@ delete from smjer where naziv = "Serviser";
 select a.naziv as grupa, b.naziv as smjer
 from grupa a inner join smjer b
 on a.smjer = b.sifra;
+
+insert into grupa(naziv, smjer) values
+("PP24", 1);
+
+-- using subqueries
+insert into grupa(naziv, smjer) values
+("test item", (select sifra from smjer where lower(naziv) like "%php%"));
