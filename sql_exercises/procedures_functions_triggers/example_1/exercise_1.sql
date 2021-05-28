852b912a9c09416a6a94b823120da92c1e057af8
tnebes <tomislav.nebes@gmail.com>
2021-05-28 16:36:11 +0200

    Solved exercise 1

--- sql_exercises/procedures_functions_triggers/example_1/exercise_1.sql
+++ sql_exercises/procedures_functions_triggers/example_1/exercise_1.sql
@@ -3,3 +3,16 @@ Create a function that ensures that ints between 980 and 5098 are entered.
 Use this function in one query, procedure or trigger.
 */
 
+use example_1;
+
+delimiter $$
+create function return_between(my_int int) returns int
+	begin
+		if (my_int > 950 and my_int < 5098) then
+			return my_int;
+		else
+			return 0; -- this is not great.
+		end if;
+	end
+$$
+delimiter ;
