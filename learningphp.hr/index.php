96a95c68c841fce9c5a2da4bcc2b20441c057d48
tnebes <tomislav.nebes@gmail.com>
2021-06-02 20:15:24 +0200

    Added foundation and basic PHP script

---
+++ learningphp.hr/index.php
@@ -0,0 +1,31 @@
+
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <link rel="stylesheet" href="assets/css/foundation.css">
+  </head>
+<body>
+
+    <div class="grid-container">
+      <?php
+        require_once 'menu.php';
+      ?>
+      <div class="grid-x grid-padding-x">
+        <div class="large-6 cell">
+          <h1>Foundation</h1>
+        </div>
+        <div class="large-6 cell">
+          <h1>Welcome to Foundation</h1>
+        </div>
+      </div>
+    </div>
+
+<script src="assets/js/vendor.js"></script>
+<script src="assets/js/foundation.js"></script>
+<script src="assets/js/app.js"></script>
+</body>
+</html>
