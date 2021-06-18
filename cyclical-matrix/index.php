e6a86ee0bf11b797ac14d1f53740e835936d1694
tnebes <tomislav.nebes@gmail.com>
2021-06-18 16:57:40 +0200

    Added the skeleton of the web application, started working on filling up an array and drawing the necessary shapes

--- cyclical-matrix/index.php
+++ cyclical-matrix/index.php
@@ -1,3 +1,9 @@
+<!--
+    Author: tnebes
+    18 June 2021
+    Cyclical matrix exercise
+-->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,13 +21,36 @@
         <p id="explanation">
             The goal of this exercise is to create an application which will generate a cyclical matrix.<br />
             The cyclical matrix has an input of two numbers. The numbers specify the width and height of the matrix.<br />
-            The application will, upon clicking "submit", create a cyclical matrix which starts in the bottom right.<br />
+            The application will, upon clicking "Generate matrix", create a cyclical matrix which starts in the bottom right.<br />
             The matrix will fill out the squares with numbers from 1 to 'n' where 'n' is the product of the two numbers supplied by the user.<br />
             The matrix will be filled out in a manner similar to a spiral.
         </p>
-        <?php
-        require 'matrix.php';
-        ?>
+    </div>
+    <div class="main">
+        <div class="input">
+            <form method="get">
+                <div>
+                    <p>
+                        Rows:
+                    </p>
+                    <input type="text" size="20" name="columns">
+                </div>
+                <div>
+                    <p>
+                        Columns:
+                    </p>
+                    <input type="text" size="20" name="rows">
+                </div>
+                <input type="submit" value="Generate matrix" style="margin-top: 2em;">                
+            </form>
+        </div>
+        <div class="output">
+            <div class="matrixContainer">
+                <?php
+                require 'matrix.php';
+                ?>
+            </div>
+        </div>
     </div>
 </body>
 </html>
--- cyclical-matrix/matrix.php
+++ cyclical-matrix/matrix.php
@@ -1,3 +1,56 @@
-<?php
+<!-- 
+    Author: tnebes
+    18 June 2021
+    Cyclical matrix
+ -->
+
+<?php declare(strict_types = 1);
+
+    main();
+
+    function main() : void 
+    {
+        if (checkInput() != 0)
+        {
+            exit(1); // something went wrong.
+        }
+        $columns = $_GET['columns'];
+        $rows = $_GET['rows'];
+        $desiredNumber = $columns * $rows;
+
+        while ($desiredNumber != 0)
+        {
+
+        }
+
+    }
+
+    /**
+     * Input is the GET from URL.
+     * 
+     * Output:
+     * 0 - all ok
+     * 1 - no input given
+     * 2 - input is invalid (negative or gibberish)
+     */
+    function checkInput() : int
+    {
+        global $_GET;
+        if (!(isset($_GET['columns']) && isset($_GET['rows'])))
+        {
+            // echo 'Please provide input.';
+            return 1; // no input given
+        }
+
+        $columns = $_GET['columns'];
+        $rows = $_GET['rows'];
+      
+        if ((int) $columns <= 0 || (int) $rows <= 0)
+        {
+            // echo 'Input is not valid.';
+            return 2; // input not valid 
+        }
+        return 0;
+    }
 
 ?>
