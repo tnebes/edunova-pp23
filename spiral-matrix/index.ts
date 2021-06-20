e232c449687b935d3cb7385571926c4bda8e32bc
tnebes <tomislav.nebes@gmail.com>
2021-06-20 18:31:17 +0200

    Animation works now maybe

--- spiral-matrix/index.ts
+++ spiral-matrix/index.ts
@@ -82,21 +82,20 @@ function main(): void
      * @param rows 
      * @param number 
      */
-    function animateCells(rows: HTMLCollectionOf<Element>, number: number): void
-    {
-        var waitTime = 250;
-        var minColumn = 0;
-        var minRow = 0;
-        var maxColumn = rows.length - 1;
-        var maxRow = rows[0].getElementsByClassName("matrixContent").length - 1;
-        var matrixContents: Array<Array<Element>> = getMatrixContents(rows);
-        var counter = 0;
-
+     async function animateCells(rows: HTMLCollectionOf<Element>, number: number)
+     {
+        let minColumn = 0;
+        let minRow = 0;
+        let maxColumn = rows.length - 1;
+        let maxRow = rows[0].getElementsByClassName("matrixContent").length - 1;
+        let matrixContents: Array<Array<Element>> = getMatrixContents(rows);
+        let counter = 0;
+ 
         while (true)
         {
             for (let j = maxRow; j >= minRow; j--)
             {
-                blinkContent(matrixContents[maxColumn][j]);
+                await blinkContent(matrixContents[maxColumn][j]);
                 counter++;
                 if (counter == number)
                 {
@@ -106,7 +105,7 @@ function main(): void
             maxColumn--;
             for (let i = maxColumn; i >= minColumn; i--)
             {
-                blinkContent(matrixContents[i][minRow])
+                await blinkContent(matrixContents[i][minRow])
                 counter++;
                 if (counter == number)
                 {
@@ -116,7 +115,7 @@ function main(): void
             minRow++;
             for (let j = minRow; j <= maxRow; j++)
             {
-                blinkContent(matrixContents[minColumn][j]);
+                await blinkContent(matrixContents[minColumn][j]);
                 counter++;
                 if (counter == number)
                 {
@@ -126,7 +125,7 @@ function main(): void
             minColumn++;
             for (let i = minColumn; i <= maxColumn; i++)
             {
-                blinkContent(matrixContents[i][maxRow]);
+                await blinkContent(matrixContents[i][maxRow]);
                 counter++;
                 if (counter == number)
                 {
@@ -136,10 +135,14 @@ function main(): void
             maxRow--;
         }
     }
-
+ 
     function blinkContent(content:Element)
     {
-        content.setAttribute("style", "background-color: rgb(0, 255, 0); color: rgb(0, 0, 0)");
+        const waitTime = 250;
+        return new Promise(resolve => {
+            content.setAttribute("style", "background-color: rgb(0, 255, 0); color: rgb(0, 0, 0)");
+            setTimeout(resolve, waitTime);
+        });
     }
-
+    
 }
