9f5280ab9e0e9a58f5ddcf8463bf48a54091ce16
tnebes <tomislav.nebes@gmail.com>
2021-07-01 11:04:20 +0200

    Added the exercise regarding recursive functions

---
+++ learningphp.hr/recursion.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types = 1);
+
+   $myNumber = 100;
+   $mySum = 0;
+
+   function recursiveSum(int $number, int $sum) : int
+   {
+      if ($number == 1)
+      {
+         return $sum += $number;
+      }
+
+      $sum += $number;
+      return recursiveSum($number - 1, $sum);
+   }
+
+   echo(recursiveSum($myNumber, $mySum));
+?>
