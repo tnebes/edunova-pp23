21c752867ad2ab4f0f3c9b085106d84eb9a8ba7a
tnebes <tomislav.nebes@gmail.com>
2021-07-06 16:31:00 +0200

    Love calculator finished

---
+++ learningphp.hr/love-calculator/index.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Love calculator</title>
+      <link rel="stylesheet" href="./assets/css/style.css">
+   </head>
+
+   <body>
+      <div class="page-content">
+         <div class="page-title">
+         Love Test
+            <div class = "page-explanation">
+               <p>
+                  Write your and your crush's name.<br />
+                  Our magical website will calculate how much in love you two are!<br />
+                  The higher the percentage, the more you love each other!
+               </p>
+            </div>
+         </div>
+         <div class="love-calculator">
+            <div class="love-calculator-main">
+               <form action="index.php" method="post">
+                  <label for="Your name">
+                     <input type="text" name="firstName" id="firstName" placeholder="Name">
+                  </label>
+                  &
+                  <label for="Your crush's name">
+                     <input type="text" name="secondName" id="secondName" placeholder="Name">
+                  </label>
+                  <button type="submit">Calculate <span id="heart">❤</span></button>
+               </form>
+            </div>
+            <div class="love-calculator-content">
+               <?php
+                  include_once 'loveCalculator.php';
+               ?>
+               <div>
+                  <img class="deco-images" id="heart" src="./assets/img/like.png">
+               </div>
+               <div>
+                  <img class="deco-images" id="butterflies" src="./assets/img/butterflies.png">
+               </div>   
+            </div>
+         </div>
+         <div id="must-have">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      </div>
+   </body>
+
+</html>
---
+++ learningphp.hr/love-calculator/loveCalculator.php
@@ -0,0 +1,136 @@
+<?php declare(strict_types = 1);
+   /**
+    * Function returns the combined names that are lowercase and that are used when calculating the love between two names
+    */
+   function getCombinedNames(string $name0, string $name1)
+   {
+      return strtolower(trim($name0)) . strtolower(trim($name1));
+   }
+
+   /**
+    * The function returns an associate array whose key is the letter and the value is the number of occurences of such a letter.
+    */
+   function getCharacterCounts(string $name0, string $name1): array
+   {
+      $names = getCombinedNames($name0, $name1);
+      $letters = [];
+
+      for ($i = 0; $i < strlen($names); $i++) {
+         $letters[$names[$i]] = returnCharacterCount($names[$i], $names);
+      }
+
+      return $letters;
+   }
+
+   /**
+    * The function returns a number which represents how often a letter occurs in a given string
+    */
+   function returnCharacterCount(string $character, $string): int
+   {
+      $counter = 0;
+
+      for ($i = 0; $i < strlen($string); $i++) {
+         if ($character == $string[$i]) {
+            $counter++;
+         }
+      }
+      return $counter;
+   }
+
+   /**
+    * Function prints the first and second name which should not be used in calculations but rather in
+    * the content of the webpage.
+    */
+   function printNameCounter(string $name0, string $name1): void
+   {
+      $names = $name0 . ' & ' . $name1;
+      echo $names . '<br />';
+   }
+
+   /**
+    * Function returns a string whose indeces correspond to the concatenated first and second name with a whitespace between them.
+    * for 'Tom Moira' the return value ought to be '12222111'
+    * Tom Moira
+    * 122 22111
+    */
+   function getNumbers(string $name0, string $name1, array $counter): string
+   {
+      $numbers = '';
+      $names = getCombinedNames($name0, $name1);
+
+      for ($i = 0; $i < strlen($names); $i++) {
+         $numbers .= $counter[$names[$i]];
+      }
+      return $numbers;
+   }
+
+   /**
+    * The recursive function that is responsible for printing the love between the two names.
+    * Function calls itself until the love is smaller than 2 chars.
+    */
+   function recursiveSum(string $numbers) : void
+   {
+      if (strlen($numbers) <= 2)
+      {
+         echo $numbers;
+         return;
+      }
+      $output = '';
+      for ($i = 0, $j = strlen($numbers) - 1; $i < $j; $i++, $j--)
+      {
+         // echo (int) $numbers[$i] + (int) $numbers[$j];
+         $output .= (int) $numbers[$i] + (int) $numbers[$j];
+      }
+      if (strlen($numbers) % 2 == 1)
+      {
+         $middle = (int) (strlen($numbers) / 2);
+         // echo $numbers[$middle];
+         $output .= $numbers[$middle];
+      }
+      // echo '<br />';
+      recursiveSum($output);
+   }
+
+   /**
+    * Function checks whether the post data is set and whether it is not empty.
+    * If empty or not set, returns false, if OK, returns true.
+    */
+   function checkPOST() : bool
+   {
+      if (!(isset($_POST['firstName']) && isset($_POST['secondName'])))
+      {
+         return false; // not set
+      }
+      if (trim($_POST['firstName']) == '' && trim($_POST['secondName']) == '')
+      {
+         return false; // empty
+      }
+      return true;
+   }
+
+   function printDecorations() : void
+   {
+
+   }
+
+   if (!checkPOST())
+   {
+      exit(1);
+   }
+
+   $firstName = trim($_POST['firstName']);
+   $secondName = trim($_POST['secondName']);
+
+   $counter = getCharacterCounts($firstName, $secondName);
+   $number = getNumbers($firstName, $secondName, $counter);
+
+   echo '<span class="lovers">';
+   printNameCounter($firstName, $secondName);
+   echo '</span>';
+   echo '<span class="special-text">';
+   recursiveSum($number);
+   echo "%\n</span>";
+
+   printDecorations();
+
+   ?>
